2d3c2bc8b2ed3255d1d6fb4b39063a017b588258
신용진 <novall9633@naver.com>
2024-02-26 00:59:33 +0900

    포트폴리오 수정

--- 포트폴리오/yj-portfolio/src/components/modules/Exp.jsx
+++ 포트폴리오/yj-portfolio/src/components/modules/Exp.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import { expData } from "../../data/expData";
 
-
 export function Exp({ cat }) {
     return (
         <div name={cat} className="bg-gradient-to-b from-black to-gray-800 text-white w-full pt-20">
             <div className="max-w-screen-lg mx-auto flex flex-col justify-around h-full px-4">
                 <div className="py-8 px-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                        프로젝트 명/{expData[cat].pj === "개인" ? "개인 프로젝트" : "단체 프로젝트"}
+                    </p>
+                    <p className="py-6 text-2xl">{expData[cat].subject}</p>
+                </div>
+                <div className="py-8 px-8">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">프로젝트 소개</p>
-                    <p className="py-6">{expData[cat].intro}</p>
+                    <p className="py-6 text-xl">{expData[cat].intro}</p>
                 </div>
                 <div className="py-8 px-8">
-                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">기술 스택</p>
-                    <p className="py-6">{expData[cat].stack}</p>
-                </div>
-                <div className="py-8 px-8">
-                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">개발 과정</p>
-                        {expData[cat].develop.map(({ sub, exp },i) => {
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">개발 기술</p>
+                    <ul className="flex gap-3 flex-wrap">
+                        {expData[cat].stack.map((v, i) => {
                             return (
-                                <>
-                                    <li className="text-2xl py-4" key={i}>{sub} - {exp}</li>
-                                </>
+                                <li className="my-4 h-8 px-1 border-2 rounded-lg text-xl" key={i}>
+                                    {v}
+                                </li>
                             );
                         })}
+                    </ul>
                 </div>
                 <div className="py-8 px-8">
                     <p className="text-2xl font-bold inline border-b-4 border-gray-500">결과물</p>
--- 포트폴리오/yj-portfolio/src/data/expData.js
+++ 포트폴리오/yj-portfolio/src/data/expData.js
@@ -1,20 +1,17 @@
 export const expData = {
     bowling: {
-        intro: "학원에서 진행한 첫 개인 프로젝트로, 평소 좋아하는 볼링 관련 사이트를 카피하여 진행하였습니다. 메인페이지 1개, 서브 페이지 1개 제작을 목표로 진행하였습니다.",
-        stack: "HTML, CSS, JavaScript",
-        develop: [
-            {
-                sub: "slide",
-                exp: "볼링 사이트는 볼링 관련 제품이나 볼링센터를 알려주기 위함이라는 목적이 크기 때문에 슬라이드가 어색하지 않게 하기위해 가장 신경을 써서 구현을 했습니다.",
-            }
-        ],
+        subject: "MK트레이딩 Clone",
+        pj:"개인",
+        intro: "학원에서 진행한 첫번째 개인프로젝트이며, HTML+CSS+JavaScript를 사용하여 볼링 사이트를 참고로 클론 코딩을 진행했습니다.",
+        stack: ["HTML", "CSS", "JavaScript"],
         result: ["1","2","3"],
         link:"https://novall9633.github.io/FED-RF-2023-Project-SYJ/1%EC%B0%A8%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/index.html",
     },
     nexis: {
-        intro: "학원에서 진행한 두번째 개인 프로젝트로, React를 사용하여 진행했습니다.",
-        stack: "HTML, CSS, JavaScript, jQuery, React, Context, NodeJS",
-        develop: [{ sub: "CORS 문제", exp: "호출하는 API" }],
+        subject: "가구 사이트 Clone",
+        pj:"개인",
+        intro: "학원에서 진행한 두번째 개인프로젝트로, 가구 홈페이지를 React + NodeJS + express를 이용하여 클론 사이트입니다. 반응형 웹으로 만들어져 모든 환경에서 사용가능합니다.",
+        stack: ["HTML", "CSS", "JavaScript", "jQuery", "React", "Context", "NodeJS"],
         result: ["1","2","3","4","5"],
         link:"https://novall9633.github.io/syj-react-pj/",
     },
